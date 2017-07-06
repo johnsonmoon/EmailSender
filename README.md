dd0f27f7aa6cc9043b3bbd5970b66b234fbafef0
xuyh <841846248@qq.com>
2017-07-06 21:15:07 +0800

    修改测试代码以及修改readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # EmailSender
 Email tools for java.
+
+Using java email API [javax.mail] to send email by SMTP, POP3 and IMAP.
+
+How to use:
+
+1.Get ready
+(1)Clone the project to local computer.
+(2)Make sure your computer is ready for the environment of java8 and maven building tools.
+(3)Locate to the project working directory.
+(4)Execute command: [mvn install -Dmaven.test.skip=true].(maven and jdk needed)
+(5)Found the .jar file in directory [target].
+(6)Import the .jar file into your project classpath.
+
+2.Use the tools
+
+examples:
+
+(1)Send text message
+EmailCredential credential = new EmailCredential(${emailHost}, ${portNumber}, ${protocol}, ${userAccount}, ${userAccountPassword});
+EmailTextMessage textMessage = new EmailTextMessage(${senderAddress}, ${receiverAddressArray}, ${subjectOfTheEmail}, ${contentTextMessage});
+EmailUtils.sendTextEmail(credential, textMessage);
+
+(2)Send multimedia message with attachment file.
+EmailCredential credential = new EmailCredential(${emailHost}, ${portNumber}, ${protocol}, ${userAccount}, ${userAccountPassword});
+EmailMultiMediaMessage multiMediaMessage = new EmailMultiMediaMessage();
+multiMediaMessage.setFrom(${senderAddress});
+multiMediaMessage.setTo(new String[] { ${receiverAddressArray} });
+multiMediaMessage.setSubject(${subjectOfTheEmail});
+multiMediaMessage.setContentText("<html>\n" +
+        "\t<head>\n" +
+        "\t\t<title>test email with files</title>\n" +
+        "\t</head>\n" +
+        "\n" +
+        "\t<body>\n" +
+        "\t\t<pre>\n" +
+        "\t\t\tHello\n" +
+        "\t\t\t<img src = \"cid:${id_1_contentFile}\">\n" +
+        "\t\t\t<img src = \"cid:${id_2_contentFile}\">\n" +
+        "\t\t\t<img src = \"cid:${id_3_contentFile}\">\n" +
+        "\t\t</pre>\n" +
+        "\t</body>\n" +
+        "</html>");
+multiMediaMessage.addContentFile(${id_1_contentFile}, ${contentFilePath_1});
+multiMediaMessage.addContentFile(${id_2_contentFile}, ${contentFilePath_2});
+multiMediaMessage.addContentFile(${id_3_contentFile}, ${contentFilePath_3});
+multiMediaMessage.addAttachFile(${attachmentFilePath_1});
+multiMediaMessage.addAttachFile(${attachmentFilePath_2});
+multiMediaMessage.addAttachFile(${attachmentFilePath_3});
+EmailUtils.sendMultiMediaEmail(credential, multiMediaMessage);
--- src/main/java/xuyihao/email/sender/Main.java
+++ src/main/java/xuyihao/email/sender/Main.java
@@ -13,6 +13,8 @@ public class Main {
 	private static String verifyInformation = "Hello! Johnson!";
 
 	public static void main(String... args) throws Exception {
+		test1();
+		test2();
 		test3();
 	}
 
@@ -24,15 +26,41 @@ public class Main {
 		EmailUtils.sendTextEmail(credential, textMessage);
 	}
 
+	public static void test2() throws Exception {
+		EmailCredential credential = new EmailCredential("to be modified", 25, EmailProtocol.IMAP, "to be modified",
+				"to be modified");
+		EmailTextMessage textMessage = new EmailTextMessage("to be modified", new String[] { "to be modified" },
+				"hello", verifyInformation);
+		EmailUtils.sendTextEmail(credential, textMessage);
+	}
+
 	public static void test3() throws Exception {
-		EmailCredential credential = new EmailCredential("", 25, EmailProtocol.IMAP, "", "");
+		EmailCredential credential = new EmailCredential("to be modified", 25, EmailProtocol.IMAP, "to be modified",
+				"to be modified");
 		EmailMultiMediaMessage multiMediaMessage = new EmailMultiMediaMessage();
-		multiMediaMessage.setFrom("");
-		multiMediaMessage.setTo(new String[] { "" });
+		multiMediaMessage.setFrom("to be modified");
+		multiMediaMessage.setTo(new String[] { "to be modified" });
 		multiMediaMessage.setSubject("Test multimedia email...");
-		multiMediaMessage.setContentText("");
-		multiMediaMessage.addContentFile("", "");
-		multiMediaMessage.addAttachFile("");
+		multiMediaMessage.setContentText("<html>\n" +
+				"\t<head>\n" +
+				"\t\t<title>内含附件,图文并茂的邮件测试</title>\n" +
+				"\t</head>\n" +
+				"\n" +
+				"\t<body>\n" +
+				"\t\t<pre>\n" +
+				"\t\t\t你好\n" +
+				"\t\t\t<img src = \"cid:photo1\">\n" +
+				"\t\t\t<img src = \"cid:photo2\">\n" +
+				"\t\t\t<img src = \"cid:photo3\">\n" +
+				"\t\t</pre>\n" +
+				"\t</body>\n" +
+				"</html>");
+		multiMediaMessage.addContentFile("photo1", "C:\\Users\\Administrator\\Desktop\\test.png");
+		multiMediaMessage.addContentFile("photo2", "C:\\Users\\Administrator\\Desktop\\test2.png");
+		multiMediaMessage.addContentFile("photo3", "C:\\Users\\Administrator\\Desktop\\test3.png");
+		multiMediaMessage.addAttachFile("C:\\Users\\Administrator\\Desktop\\test.doc");
+		multiMediaMessage.addAttachFile("C:\\Users\\Administrator\\Desktop\\test2.doc");
+		multiMediaMessage.addAttachFile("C:\\Users\\Administrator\\Desktop\\test3.doc");
 		EmailUtils.sendMultiMediaEmail(credential, multiMediaMessage);
 	}
 }
